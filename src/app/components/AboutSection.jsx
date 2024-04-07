2463cf7ac2fadc4e15af45a68d79027cbf543297
projectnext.js <projectnext.js.in>
2024-04-07 15:09:01 +0530

    intial msg

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -10,9 +10,9 @@ const TAB_DATA = [
     content: (
       <ul className="list-disc pl-2">
         <li>Node.js</li>
-        <li>Express</li>
+       
         <li>PostgreSQL</li>
-        <li>Sequelize</li>
+       
         <li>JavaScript</li>
         <li>React</li>
       </ul>
@@ -23,8 +23,8 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Fullstack Academy of Code</li>
-        <li>University of California, Santa Cruz</li>
+        <li>BCA (Science) Grduated</li>
+        <li>University of SPPU</li>
       </ul>
     ),
   },
@@ -33,8 +33,8 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>AWS Cloud Practitioner</li>
-        <li>Google Professional Cloud Developer</li>
+        <li>Poster Presentation Practitioner</li>
+        <li> Web Developer Certification</li>
       </ul>
     ),
   },
@@ -59,8 +59,8 @@ const AboutSection = () => {
           <p className="text-base lg:text-lg">
             I am a full stack web developer with a passion for creating
             interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
+            working with JavaScript, React,Node.js, PostgreSQL,
+            HTML, CSS, and Git. I am a quick learner and I am always
             looking to expand my knowledge and skill set. I am a team player and
             I am excited to work with others to create amazing applications.
           </p>
